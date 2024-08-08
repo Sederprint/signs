44cbcf0b57b55440db34beb5ac94b99814cd23d1
sederprint <sederprint@gmail.com>
2024-08-08 18:36:58 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,350 @@
+const signInfo = [
+  {
+    id: "signA",
+    coordinates: [31.724133, 35.220021],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign A",
+    status: "available",
+  },
+  {
+    id: "signB",
+    coordinates: [31.724597, 35.220637],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signC",
+    coordinates: [31.724902, 35.219865],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signD",
+    coordinates: [31.721955, 35.220166],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signE",
+    coordinates: [31.722985, 35.22232],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signF",
+    coordinates: [31.72417, 35.226478],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signG",
+    coordinates: [31.722821, 35.225365],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signH",
+    coordinates: [31.722821, 35.225365],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signI",
+    coordinates: [31.722308, 35.22395],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signJ",
+    coordinates: [31.721451, 35.223316],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signK",
+    coordinates: [31.71995, 35.220785],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signL",
+    coordinates: [31.721699, 35.216498],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signM",
+    coordinates: [31.722893, 35.216375],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signN",
+    coordinates: [31.725291, 35.217609],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signO",
+    coordinates: [31.72376, 35.217394],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signP",
+    coordinates: [31.726986, 35.21763],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signQ",
+    coordinates: [31.72673, 35.224407],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signR",
+    coordinates: [31.726507, 35.224387],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signS",
+    coordinates: [31.726281, 35.223849],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signT",
+    coordinates: [31.726607, 35.223298],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signU",
+    coordinates: [31.727181, 35.221157],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signV",
+    coordinates: [31.727181, 35.221157],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signW",
+    coordinates: [31.725431, 35.223505],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signX",
+    coordinates: [31.724411, 35.224333],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signY",
+    coordinates: [31.725178, 35.224307],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "signZ",
+    coordinates: [31.72481, 35.223946],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "sign1",
+    coordinates: [31.73222, 35.219594],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+  {
+    id: "sign2",
+    coordinates: [31.731358, 35.219141],
+    price: 295,
+    droneMovie: "URL_TO_DRONE_MOVIE",
+    signImage: "URL_TO_SIGN_IMAGE",
+    description: "Description for sign B",
+    status: "not available",
+  },
+];
+
+//constant map
+
+const mainMap = L.map("constantMap").setView([31.7359, 35.2267], 15);
+
+const bounds = [
+  [31.732, 35.2175], // Adjusted Northwest corner
+  [31.715, 35.232], // Adjusted Southeast corner
+];
+
+mainMap.fitBounds(bounds);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+}).addTo(mainMap);
+
+signInfo.forEach((sign) => {
+  const marker = L.marker(sign.coordinates).addTo(mainMap);
+
+  const popupContent = `
+      <div>
+        <h3>שלט ${sign.id}</h3>
+        <p>${sign.description}</p>
+        <img src="${sign.signImage}" alt="תמונה של שלט ${sign.id}" style="width: 100px; height: auto;">
+        <p>מחיר: ${sign.price} ש"ח</p>
+        <p>סטטוס: ${sign.status}</p>
+        <a href="${sign.droneMovie}" target="_blank">סרטון רחפן</a>
+      </div>
+    `;
+
+  marker.bindPopup(popupContent);
+
+  marker.on("click", () => {
+    marker.openPopup();
+  });
+});
+
+//end of constant map
+
+mapContainer = document.getElementById("mapContainer");
+const openMapButton = document.getElementById("openMapButton");
+const closeMapButton = document.getElementById("closeMapButton");
+
+openMapButton.addEventListener("click", () => {
+  mapContainer.classList.add("fullscreen");
+  map.invalidateSize();
+  map.setView([31.7359, 35.2267], 17); // Adjust zoom level as needed
+});
+
+closeMapButton.addEventListener("click", () => {
+  mapContainer.classList.remove("fullscreen");
+  map.invalidateSize();
+  map.setView([31.715, 35.232], 15); // Reset to original zoom level
+});
+
+const map = L.map("map").setView([31.7359, 35.2267], 15);
+
+map.setMaxBounds(bounds);
+map.fitBounds(bounds);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+}).addTo(map);
+
+signInfo.forEach((sign) => {
+  const marker = L.marker(sign.coordinates).addTo(map);
+  marker.on("click", () => {
+    const popupContent = `
+      <div>
+        <h3>שלט ${sign.id}</h3>
+        <p>${sign.description}</p>
+        <img src="${sign.signImage}" alt="תמונה של שלט ${sign.id}" style="width: 100px; height: auto;">
+        <p>מחיר: ${sign.price} ש"ח</p>
+        <p>סטטוס: ${sign.status}</p>
+        <a href="${sign.droneMovie}" target="_blank">סרטון רחפן</a>
+      </div>
+    `;
+    marker.bindPopup(popupContent).openPopup();
+  });
+});
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
